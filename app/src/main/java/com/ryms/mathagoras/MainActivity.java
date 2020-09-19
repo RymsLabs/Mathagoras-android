54c404e02c479e7874b5721c235487b400815ca5
Sahajpal <sahajpal80@gmail.com>
2020-09-19 23:53:03 +0530

    Completed Welcome activity

--- app/src/main/java/com/ryms/mathagoras/MainActivity.java
+++ app/src/main/java/com/ryms/mathagoras/MainActivity.java
@@ -2,13 +2,31 @@ package com.ryms.mathagoras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button explore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        explore=(Button)findViewById(R.id.explore);
+        explore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+
+    public void openNewActivity(){
+        Intent intent = new Intent(this, OnBoardingActivity.class);
+        startActivity(intent);
     }
 }

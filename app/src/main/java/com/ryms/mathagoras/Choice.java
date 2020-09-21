bb797348fd4e41debe7d759625ed764267872412
Sahajpal <sahajpal80@gmail.com>
2020-09-21 09:24:31 +0530

    Login Activity Framework is ready

--- app/src/main/java/com/ryms/mathagoras/Choice.java
+++ app/src/main/java/com/ryms/mathagoras/Choice.java
@@ -2,13 +2,42 @@ package com.ryms.mathagoras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Choice extends AppCompatActivity {
 
+    Button login, signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+
+        login=(Button)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openLoginActivity();
+        }
+    });
+
+        signup=(Button)findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignActivity();
+            }
+        });
+    }
+
+    public void openLoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+    public void openSignActivity(){
+        Intent intent = new Intent(this, OnBoardingActivity.class);
+        startActivity(intent);
     }
 }

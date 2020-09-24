7d55724f6c45ca0e44ee5487f66d5bb200e61869
Sahajpal <sahajpal80@gmail.com>
2020-09-24 12:39:56 +0530

    Completed signup and login layouts

--- app/src/main/java/com/ryms/mathagoras/Choice.java
+++ app/src/main/java/com/ryms/mathagoras/Choice.java
@@ -37,7 +37,7 @@ public class Choice extends AppCompatActivity {
         startActivity(intent);
     }
     public void openSignActivity(){
-        Intent intent = new Intent(this, OnBoardingActivity.class);
+        Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
     }
 }
